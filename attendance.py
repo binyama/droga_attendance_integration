ff700ad7989d781aa4d0dd406894f8ad377d4296
Binyam Assefa <binyam@afomsoft.com>
2023-07-31 14:56:50 +0300

    Attendance fetch method updated

--- attendance.py
+++ attendance.py
@@ -1,16 +1,18 @@
-import xmlrpc.client
-import helper
+import time
 from datetime import datetime
+
 import psycopg2
 import psycopg2.extras
-import time
-import lxml
+
+import helper
+
 
 def import_attendance():
     try:
+        dt = datetime.utcnow()
 
-        today = datetime.utcnow().strftime("%Y-%m-%d H%:M%:S%")
-        today = '2023-07-08'
+        formatted_date = dt.strftime("%Y-%m-%d %H:%M:%S")
+        today = '2023-06-08'
         cursor = helper.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
 
         cursor.execute(
@@ -65,7 +67,84 @@ def import_attendance():
 
 
     except Exception as e:
-        print('Error for employee ', record['emp_code'])
+        print('Error for employee ', e)
+
+
+def import_attendance_json():
+    dt = datetime.utcnow()
+
+    today = dt.strftime("%Y-%m-%d %H:%M:%S")
+    # today = '2023-06-08'
+    cursor = helper.conn.cursor(cursor_factory=psycopg2.extras.RealDictCursor)
+
+    cursor.execute(
+        "select ap.id as machine_trans_id,pe.id,pe.emp_code,pe.first_name ,pe.last_name,ap.att_date ,ap.week ,ap.weekday ,ap.clock_in ,ap.in_date ,ap.in_time ,"
+        "ap.clock_out,ap.out_date ,ap.out_time ,ap.workday from personnel_employee pe inner join att_payloadparing ap on pe.id=ap.emp_id "
+        "where ap.att_date >='" + today + "'")
+
+    records = cursor.fetchall()
+
+    try:
+        for record in records:
+            empid = get_emp_id(record['emp_code'])
+
+            # search empolyee record in odoo
+            # employee = helper.models.execute_kw(helper.odoo_db, helper.uid, helper.odoo_password, 'hr.employee',
+            # 'search_read', [[['barcode', '=', empid]]])
+
+            employee = helper.call(helper.url_json, "object", "execute", helper.odoo_db, helper.uid_json,
+                                   helper.odoo_password, 'hr.employee', 'search_read', [('barcode', '=', empid)])
+
+            for emp in employee:
+                # get odoo employee id
+                if emp['id'] is None or record['id'] is None:
+                    continue
+
+                odoo_emp_id = emp['id']
+                attendance_machine_trans_id = record['machine_trans_id']
+                clock_in = record['clock_in']
+                clock_out = record['clock_out']
+
+                if clock_in is None:
+                    clock_in = False
+                else:
+                    clock_in = clock_in.strftime("%Y-%m-%d %H:%M:%S")
+
+                if clock_out is None:
+                    clock_out = False
+                else:
+                    clock_out = clock_out.strftime("%Y-%m-%d %H:%M:%S")
+
+                # check existing attendance
+
+                attendance = helper.call(helper.url_json, "object", "execute", helper.odoo_db,
+                                         helper.uid_json,
+                                         helper.odoo_password, 'hr.attendance', 'search_read',
+                                         [('attendance_machine_trans_id', '=', attendance_machine_trans_id)])
+
+                if len(attendance) == 0:  # insert new record
+                    if clock_in is not None or odoo_emp_id != ' ':
+                        helper.call(helper.url_json, "object", "execute", helper.odoo_db,
+                                    helper.uid_json,
+                                    helper.odoo_password, 'hr.attendance', 'create',
+                                    [{'employee_id': odoo_emp_id, 'check_in': clock_in,
+                                      'check_out': clock_out,
+                                      'attendance_machine_trans_id': attendance_machine_trans_id}]
+                                    )
+                else:  # update the record
+                    if clock_out is not None or odoo_emp_id != ' ' or (clock_out > clock_in):
+                        attendance_id = attendance[0]['id']
+                        helper.call(helper.url_json, "object", "execute", helper.odoo_db,
+                                    helper.uid_json,
+                                    helper.odoo_password, 'hr.attendance', 'write', [attendance_id],
+                                    {'check_out': clock_out,
+                                     'attendance_machine_trans_id': attendance_machine_trans_id}
+                                    )
+
+
+    except Exception as e:
+        print('Error for employee ', e)
+
 
 def get_emp_id(emp_id):
     if len(emp_id) == 1:
@@ -81,7 +160,7 @@ def get_emp_id(emp_id):
 
 def main():
     while True:
-        import_attendance()
+        import_attendance_json()
         time.sleep(helper.service_run_time)
 
 
--- helper.py
+++ helper.py
@@ -1,6 +1,8 @@
 import configparser
 import xmlrpc.client
-
+import random
+import json
+import urllib.request
 import psycopg2
 
 # CREATE OBJECT
@@ -37,3 +39,26 @@ conn = psycopg2.connect(database=attendance_db,
                         user=attendance_username,
                         password=attendance_password,
                         port=attendance_port)
+
+
+def json_rpc(url, method, params):
+    data = {
+        "jsonrpc": "2.0",
+        "method": method,
+        "params": params,
+        "id": random.randint(0, 1000000000),
+    }
+    req = urllib.request.Request(url=url, data=json.dumps(data).encode(), headers={
+        "Content-Type":"application/json",
+    })
+    reply = json.loads(urllib.request.urlopen(req).read().decode('UTF-8'))
+    if reply.get("error"):
+        raise Exception(reply["error"])
+    return reply["result"]
+
+def call(url, service, method, *args):
+    return json_rpc(url, "call", {"service": service, "method": method, "args": args})
+
+# log in the given database
+url_json = "%s/jsonrpc" % (odoo_url)
+uid_json = call(url_json, "common", "login", odoo_db, odoo_username, odoo_password)
